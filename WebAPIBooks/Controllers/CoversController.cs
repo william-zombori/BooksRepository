ba3574e9f9399aa077a3a708e53ba08b2718361b
william <william.zombori@gmail.com>
2023-10-22 17:30:48 +0300

    #wip: Added CoversController

---
+++ WebAPIBooks/Controllers/CoversController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPIBooks.DTOs;
+using WebAPIBooks.Services.Interfaces;
+
+namespace WebAPIBooks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoversController : ControllerBase
+    {
+        private readonly ICoversService _coversService;
+
+        public CoversController(ICoversService coversService)
+        {
+            _coversService = coversService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<BookDto>> Get(Guid id)
+        {
+            var cover = await _coversService.GetCoverByIdAsync(id);
+
+            return Ok(cover);
+        }
+    }
+}
